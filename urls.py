da63f9a481734edcb8ebaabd7fb56f78b773b8e4
monnand <monnand@monnand-laptop.(none)>
2010-11-19 23:57:44 -0500

    Default page set to valid records

--- urls.py
+++ urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
+    (r'^$', 'mysite.accounting.views.show_valid_rcd'),
     (r'^hello_world/', 'mysite.testcases.hello.hello_world'),
     (r'^addpay/', 'mysite.accounting.views.add_payment'),
     (r'^allrcd/', 'mysite.accounting.views.show_all_rcd'),
