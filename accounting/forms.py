0b428950677dc565a9accfc0887f24e01ba7b4b0
root <root@dokey.(none)>
2010-11-20 00:39:47 -0500

    Changed an error

--- accounting/forms.py
+++ accounting/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class PaymentInputForm(forms.Form):
     name = forms.CharField(max_length=32)
-    amount = forms.FloatField(min_val=0.00)
+    amount = forms.FloatField(min_value=0.00)
     memo = forms.CharField()
