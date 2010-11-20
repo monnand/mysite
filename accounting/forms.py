305f5ce4024346bf9f4a68f3d57a41a6b8c56952
monnand <monnand@monnand-laptop.(none)>
2010-11-20 00:38:26 -0500

    Add a name

--- accounting/forms.py
+++ accounting/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class PaymentInputForm(forms.Form):
     name = forms.CharField(max_length=32)
-    amount = forms.FloatField()
+    amount = forms.FloatField(min_val=0.00)
     memo = forms.CharField()
