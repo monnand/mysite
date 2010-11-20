fd6c2740b01e5ee72d4215bfd4b03f655873fc6e
monnand <monnand@monnand-laptop.(none)>
2010-11-19 23:15:42 -0500

    Remove models/

--- models/paymentrcd.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class PaymentRecord(models.Model):
-    name = models.CharField(max_length=32)
-    amount = models.FloatField()
-    memo = models.TextField()
-    is_valid = models.BooleanField()
-    start_time = models.DateTimeField()
