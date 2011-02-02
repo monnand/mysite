2c18cb908c23b5300c9123a7e6fb869f29fd18b5
monnand <monnand@monnand-laptop.(none)>
2011-02-02 08:10:16 -0500

    To work with lighttpd

--- urls.py
+++ urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
     (r'^$', 'mysite.accounting.views.show_valid_rcd'),
-    (r'^hello_world/', 'mysite.testcases.hello.read_header'),
+    (r'^mysite/hello_world/', 'mysite.testcases.hello.read_header'),
     (r'^addpay/', 'mysite.accounting.views.add_payment'),
     (r'^allrcd/', 'mysite.accounting.views.show_all_rcd'),
     (r'^vldrcd/', 'mysite.accounting.views.show_valid_rcd'),
